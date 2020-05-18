55d41499a1a612a0fe1c35600d3153a55586ae54
Hank.kao <07561.hank.kao@ict888.net>
2020-05-18 16:37:49 +0800

    Updates

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,28 @@
-console.log('hi');
+/* global window */
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', function (event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
+
+function openPromptToHomeScreen() {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+
+    deferredPrompt.userChoice.then(function (choiceResult) {
+      console.log(choiceResult.outcome);
+
+      if (choiceResult.outcome === 'dismissed') {
+        console.log('User cancelled installation');
+      } else {
+        console.log('User added to home screen');
+      }
+    });
+
+    deferredPrompt = null;
+  }
+}
