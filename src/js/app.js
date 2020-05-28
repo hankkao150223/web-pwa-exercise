478d996b01c2626775ea68c9e639fe98aa8c20ea
Hank.kao <07561.hank.kao@ict888.net>
2020-05-28 19:08:14 +0800

    Updates

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 let deferredPrompt;
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then(function () {
+  navigator.serviceWorker.register('./sw.js').then(function () {
     console.log('Service worker registered!');
   });
 }
