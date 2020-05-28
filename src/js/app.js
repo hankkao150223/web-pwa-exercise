23e7073214a0b45351f54b00bdb4a13d873e578a
Hank.kao <07561.hank.kao@ict888.net>
2020-05-28 18:28:51 +0800

    Updates

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,10 @@
-/* global window */
-
 let deferredPrompt;
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function () {
+    console.log('Service worker registered!');
+  });
+}
 
 window.addEventListener('beforeinstallprompt', function (event) {
   console.log('beforeinstallprompt fired');
